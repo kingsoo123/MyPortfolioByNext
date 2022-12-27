0705bc4bbd20f00582227a75abf646024ac673a7
kingsleyonyelo <sabistore12@gmail.com>
2022-12-27 21:17:00 +0100

    ficed about

--- components/workExperience.tsx
+++ components/workExperience.tsx
@@ -91,7 +91,7 @@ function WorkExperience({}: Props) {
       whileInView={{ opacity: 1, x: 0 }}
       className="h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl mt-20">
         Experience
       </h3>
       <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
