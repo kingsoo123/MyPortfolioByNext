dd3479c1c686b941ea10ca60baf764b7161c179f
kingsleyonyelo <sabistore12@gmail.com>
2022-12-27 21:21:44 +0100

    ficed about

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -57,7 +57,7 @@ function Projects({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
-      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 mt-40">
         {projects.map((project, i) => (
           <div
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
