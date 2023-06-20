9f98fbb75dc30141618bd54e90576b06b40a1825
kingsleyonyelo <sabistore12@gmail.com>
2023-06-20 11:18:26 +0100

    added urls

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 type Props = {};
 
@@ -56,12 +57,12 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-        <p
+        <Link
+          href="#contact"
           className="uppercase hidden md:inline-flex text-sm text-gray-400"
-          onClick={() => window.open("http://www.gmail.com")}
         >
           Get In Touch
-        </p>
+        </Link>
       </motion.div>
     </header>
   );
