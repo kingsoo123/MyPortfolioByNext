dcbac0b39eeba58381ac711f29b1edfbaf4a9671
kingsleyonyelo <sabistore12@gmail.com>
2023-08-01 03:57:47 +0100

    removed valley pay

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -75,7 +75,7 @@ function Skills({}: Props) {
   ];
   return (
     <motion.div
-      initial={{ opacity: 0 }}
+      initial={{ : 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
       className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
