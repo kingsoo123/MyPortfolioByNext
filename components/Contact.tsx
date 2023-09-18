17fb5a676fb803e3c62579e0896820652f637ebc
kingsleyonyelo <sabistore12@gmail.com>
2023-09-18 17:16:07 +0100

    new commit

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,7 +6,7 @@ function Contact({}: Props) {
   return (
     <div className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl justify-evenly mx-auto items-center px-10 z-0">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Contact
+        jsjd
       </h3>
       <div className="flex flex-col space-y-10 ">
         <h4 className="text-4xl font-semibold text-center">
