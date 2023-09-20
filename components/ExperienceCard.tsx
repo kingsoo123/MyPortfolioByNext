9fd78b797b7556cb40305e53003d528414a6d778
kingsleyonyelo <sabistore12@gmail.com>
2023-09-20 06:19:00 +0100

    new commit

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 
 type Props = {
-  experience: string;
+  experience: any;
   companyName: string;
   startDate: string;
   endDate: string;
