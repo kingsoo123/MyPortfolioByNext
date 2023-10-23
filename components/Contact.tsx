eec828cae4414d8c1c38140dfed2bc93793bedbd
kingsleyonyelo <sabistore12@gmail.com>
2023-10-23 09:35:16 +0100

    feattt

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -17,7 +17,7 @@ function Contact({}: Props) {
         </h4>
         <div className="flex items-center space-x-5 justify-center">
           <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-          <p>+234 </p>
+          <p>+234 9054017119 </p>
         </div>
 
         <div className="flex items-center space-x-5 justify-center">
